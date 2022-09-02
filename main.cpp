7b5e4ac97c59054dd7af39aa5df16fcf569f7551
Diego Valdivia <valdiviacox.d@northeastern.edu>
2022-09-01 23:56:35 -0400

    Collision and shape OOD implementation

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 
 using namespace std;
 
-
 /* Memory */
 #define FPGA_ONCHIP_BASE 0xC8000000
 #define FPGA_ONCHIP_SPAN 0x0003FFFF
@@ -35,6 +34,11 @@ int get_data_bits(int);
 #define PI 3.14159265
 
 
+
+
+/********************************************************************************
+* FPGA Board and VGA Monitor Class Set Up.
+*******************************************************************************/
 class DE1SoCfpga {
 
   public:
@@ -197,56 +201,291 @@ class DE1SoCfpga {
 }
 
 
+/********************************************************************************
+* Collision for Polygons Implementation.
+*******************************************************************************/
+class Point {
+  public:
+    float x, y;
+
+  Point(float x, float y) {
+    this->x = x;
+    this->y = y;
+  }
+};
+
+class Edge {
+  public:
+    float x, y;
+
+  Edge(float x, float y) {
+    this->x = x;
+    this->y = y;
+  }
+};
+
+class Polygon {
+  public:
+    vector<Point> vertices;
+    vector<Edge> edges;
+    
+  Polygon(vector<Point> vertices) {
+    this->vertices = vertices;
+
+    for (int i = 0; i < this->vertices.size(); i++) {
+      int j = (i + 1) % this->vertices.size();
+      Point a = this->vertices[i];
+      Point b = this->vertices[j];
+      Edge edge(b.x - a.x, b.y - a.y);
+      edges.push_back(edge);
+    }
+  }
+
+  /**
+  * Collision Algorithm.
+  * Itirates through this polygon against provided other polygon.
+  * Returns false if no collision has taken place.
+  **/
+  bool satCollision(Polygon* &other) {
+
+    vector<Edge> perpendicularStack;
+
+    for (int i = 0; i < this->edges.size(); i++) {
+      Edge perpendicularLine(-edges[i].y, edges[i].x);
+      perpendicularStack.push_back(perpendicularLine);
+    }
+
+    for (int i = 0; i < other->edges.size(); i++) {
+      Edge perpendicularLine(-other->edges[i].y, other->edges[i].x);
+      perpendicularStack.push_back(perpendicularLine);
+    }
+
+    for (int i = 0; i < perpendicularStack.size(); i++) {
+
+      float nothing = 50000000;
+      float aLeft = -10000000, aRight = 50000000, bLeft = -10000000, bRight = 50000000;
+      float dot;
+
+      for (int j = 0; j < this->vertices.size(); j++) {
+        dot = this->vertices[j].x * perpendicularStack[i].x +
+        this->vertices[j].y * perpendicularStack[i].y;
+
+        if (aLeft == nothing || dot > aLeft){
+          aLeft = dot;
+        }
+        if (aRight == nothing || dot < aRight){
+          aRight = dot;
+        }
+      }
+
+      for (int j = 0; j < other->vertices.size(); j++) {
+        dot = other->vertices[j].x * perpendicularStack[i].x +
+        other->vertices[j].y * perpendicularStack[i].y;
+
+        if (bLeft == nothing || dot > bLeft){
+          bLeft = dot;
+        }
+        if (bRight == nothing || dot < bRight){
+          bRight = dot;
+        }
+      }
+
+    if ((aRight < bLeft && aRight > bRight) || (bRight < aLeft && bRight > aRight)) { continue; }
+    else { return false; }
+
+    }
+    return true;
+  }
+}
 
 
 
 /********************************************************************************
-* Resamples 24-bit color to 16-bit or 8-bit color
+* Shapes
 *******************************************************************************/
+class Shape {
+  public:
+    float x, y, xv, yv, width, height, angle, angular_vel;
+    short pixel_color;
+    Polygon *p;
+    DE1SoCfpga *mem;
 
-int resample_rgb(int num_bits, int color) {
-
-  if (num_bits == 8) {
-    color = (((color >> 16) & 0x000000E0) | ((color >> 11) & 0x0000001C) | ((color >> 6) & 0x00000003));
-    color = (color << 8) | color;
-  } else if (num_bits == 16) {
-    color = (((color >> 8) & 0x0000F800) | ((color >> 5) & 0x000007E0) | ((color >> 3) & 0x0000001F));
+  Shape(float x, float y, float xv, float yv, float width, float height, float angle, float angular_vel, short pixel_color, DE1SoCfpga *mem) {
+    this->x = x;
+    this->y = y;
+    this->xv = xv;
+    this->yv = yv;
+    this->width = width;
+    this->height = height;
+    this->angle = angle;
+    this->angular_vel = angular_vel;
+    this->pixel_color = pixel_color;
+    this->mem = mem;
   }
 
-  return color;
-}
+  // Actual implementations in children class overide the inherited method.
+  virtual void draw() = 0;
+  virtual vector<Point> corners() = 0;
 
-int get_data_bits(int mode) {
+  void move() {
+    x = x + xv;
+    y = y + yv;
+    angle = (angle + angular_vel);
+    // Corner cases
+    if (angle > 180) { angle -= 360.0; }
+    if (angle < -180) { angle += 360.0; }
 
-  switch (mode) {
-    case 0x0:
-      return 1;
-    case 0x7:
-      return 8;
-    case 0x11:
-      return 8;
-    case 0x12:
-      return 9;
-    case 0x14:
-      return 16;
-    case 0x17:
-      return 24;
-    case 0x19:
-      return 30;
-    case 0x31:
-      return 8;
-    case 0x32:
-      return 12;
-    case 0x33:
-      return 16;
-    case 0x37:
-      return 32;
-    case 0x39:
-      return 40;
+    p = new Polygon(corners());
   }
-}
+
+  void bounceHorizontal() { xv = -xv;}
+
+  void bounceVertical() {yv = -yv;}
+
+  void horizontalcollision(Shape* other) {
+    if (p->satCollision(other->p) == 1) {
+      bounceHorizontal();
+      angular_vel = -angular_vel;
+      move();
+    }
+  }
+
+  void verticalcollision(Shape* other) {
+    if (p->satCollision(other->p) == 1) {
+      bounceVertical();
+      angular_vel = -angular_vel;
+      move();
+    }
+  }
+
+  void checkcollision(Shape* other) {
+
+    if (p->satCollision(other->p) == 1) {
+
+      float diffX = x - other->x;
+      float diffY = y - other->y;
+      float diffM = sqrt(diffX*diffX + diffY*diffY);
+      diffX /= diffM;
+      diffY /= diffM;
+
+      float thisM = sqrt(xv*xv + yv*yv);
+      this->yv = diffY * thisM;
+      float otherM = sqrt(other->xv*other->xv + other->yv*other->yv);
+      other->xv = -diffX * otherM;
+      other->yv = -diffY * otherM;
+
+      move();
+      other->move();
+    }
+  }
+};
 
 
+class Square : public Shape {
+  public:
+
+  Square(float x, float y, float xv, float yv, float width, float height, float angle, float angular_vel, short pixel_color, DE1SoCfpga *mem) 
+    : Shape(x, y, xv, yv, width, height, angle, angular_vel, pixel_color, mem) {
+      p = new Polygon(corners());
+  }
+
+  void draw() {
+    mem->video_box(x, y, width, height, angle, pixel_color);
+  }
+
+  vector<Point> corners() {
+
+    vector<Point> corner;
+    float a, b;
+    float vec_x, vec_y;
+    float cox, coy, c1x, c1y, c3x, c3y;
+
+    vec_x = cos(angle*PI/ 180.0) / 2;
+    vec_y = sin(angle*PI / 180.0) / 2;
+    // Perp Vector to draw
+    float pvc_x, pvc_y;
+    pvc_x = -vec_y;
+    pvc_y = vec_x;
+
+    a = x - ((width) * vec_x) - ((height) * pvc_x);
+    b = y - ((width) * vec_y) - ((height) * pvc_y);
+    cox = (a + ((width*2)*vec_x) + ((height*2)*pvc_x));
+    coy = (b + ((width*2)*vec_y) + ((height*2)*pvc_y));
+    c1x = a + (width*2*vec_x);
+    c1y = b + (width*2*vec_y);
+    c3x = a + (height*2*pvc_x);
+    c3y = b + (height*2*pvc_y);
+
+    Point a1(a, b);
+    Point a2(c1x, c1y);
+    Point a3(cox, coy);
+    Point a4(c3x, c3y);
+
+    corner.push_back(a1);
+    corner.push_back(a2);
+    corner.push_back(a3);
+    corner.push_back(a4);
+
+    return corner;
+  }
+};
+
+
+class Triangle : public Shape {
+  public:
+
+  Triangle(float x, float y, float xv, float yv, float width, float height, float angle, float angular_vel, short pixel_color, DE1SoCfpga *mem) 
+    : Shape(x, y, xv, yv, width, height, angle, angular_vel, pixel_color, mem) {
+      p = new Polygon(corners());
+  }
+
+  void draw() {
+    mem->video_t(x, y, width, height, angle, pixel_color);
+  }
+
+  vector<Point> corners() {
+
+    vector<Point> corner;
+    float h, a, b;
+    float vec_x, vec_y;
+    float cox, coy, c1x, c1y;
+
+    vec_x = cos(angle*PI/ 180.0) / 2;
+    vec_y = sin(angle*PI / 180.0) / 2;
+    // Perp Vector to draw
+    float pvc_x, pvc_y;
+    pvc_x = -vec_y;
+    pvc_y = vec_x;
+
+    a = x - ((width) * vec_x) - ((height) * pvc_x);
+    b = y - ((width) * vec_y) - ((height) * pvc_y);
+    cox = (a + ((width*2)*vec_x));
+    coy = (b + ((width*2)*vec_y));
+    c1x = a + ((width)*vec_x) - (height*2*pvc_x);
+    c1y = b + ((width)*vec_y) - (height*2*pvc_y);
+
+    Point a1(a, b);
+    Point a2(cox, coy);
+    Point a3(c1x, c1y);
+    corner.push_back(a1);
+    corner.push_back(a3);
+    corner.push_back(a2);
+    return corner;
+  }
+};
+
+
+
+
+
+/* global variables */
+int screen_x;
+int screen_y;
+int res_offset;
+int col_offset;
+/********************************************************************************
+* Main Process
+*******************************************************************************/
 int main(void) {
 
   DE1SoCfpga *main = new DE1SoCfpga;
@@ -321,7 +560,7 @@ int main(void) {
     usleep(16*1000);
     main->clear_screen();
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
       shape[i]->horizontalcollision(leftscreen);
       shape[i]->horizontalcollision(rightscreen);
       shape[i]->verticalcollision(topscreen);
@@ -335,7 +574,51 @@ int main(void) {
       shape[i]->draw();
 
       } // for loop
-
   } // while loop
-
 } // main loop
+
+
+
+/********************************************************************************
+* Resamples 24-bit color to 16-bit or 8-bit color
+*******************************************************************************/
+int resample_rgb(int num_bits, int color) {
+
+  if (num_bits == 8) {
+    color = (((color >> 16) & 0x000000E0) | ((color >> 11) & 0x0000001C) | ((color >> 6) & 0x00000003));
+    color = (color << 8) | color;
+  } else if (num_bits == 16) {
+    color = (((color >> 8) & 0x0000F800) | ((color >> 5) & 0x000007E0) | ((color >> 3) & 0x0000001F));
+  }
+  return color;
+}
+
+int get_data_bits(int mode) {
+
+  switch (mode) {
+    case 0x0:
+      return 1;
+    case 0x7:
+      return 8;
+    case 0x11:
+      return 8;
+    case 0x12:
+      return 9;
+    case 0x14:
+      return 16;
+    case 0x17:
+      return 24;
+    case 0x19:
+      return 30;
+    case 0x31:
+      return 8;
+    case 0x32:
+      return 12;
+    case 0x33:
+      return 16;
+    case 0x37:
+      return 32;
+    case 0x39:
+      return 40;
+  }
+}
